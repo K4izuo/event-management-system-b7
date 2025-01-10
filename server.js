7045b65d3f23da7f2bdd56add15fc2510a2a45c8
K4izuo <crisjutineoracion22@gmail.com>
2025-01-10 18:36:51 +0800

    commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 
 const app = express();
-const port = process.env.PORT || 10000;
+const port = 10000;
 
 // Database configuration
 const dbConfig = {
